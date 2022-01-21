e45bdf2f456ba0e72cfa3c4a1d73cae379e8bae9
Tuhar Shrivastav <ts3657@gmail.com>
2022-01-21 17:25:42 +0530

    hello world program in electron js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("electron is running");
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const windows = new BrowserWindow({
+    height: 800,
+    width: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  windows.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
