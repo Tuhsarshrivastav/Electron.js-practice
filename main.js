a28a8cb7926ed8f075a6e1e313f42a79c21d6b22
Tuhar Shrivastav <ts3657@gmail.com>
2022-01-22 09:51:15 +0530

    frame less

--- main.js
+++ main.js
@@ -4,26 +4,12 @@ function createWindow() {
   const windows = new BrowserWindow({
     height: 800,
     width: 600,
-    // frame: false,
-    // backgroundColor: "#888",
-    // title: "trying electron",
-    // alwaysOnTop: true,
-    resizable: true,
+    frame: false,
     webPreferences: {
       nodeIntegration: true,
     },
   });
-
   windows.loadFile("index.html");
-  windows.webContents.openDevTools();
 }
 
-app.on("before-quit", () => {
-  alert("you want to quit");
-});
-
-// app.whenReady().then(createWindow);
-app.on("ready", () => {
-  createWindow();
-  console.log("app is ready");
-});
+app.whenReady().then(createWindow);
